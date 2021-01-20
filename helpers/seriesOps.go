17ad15f5c0a5a999d792e0bc548a8bc9b2cffb52
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-01-20 18:43:24 +0530

    Added map function for a series

--- helpers/seriesOps.go
+++ helpers/seriesOps.go
@@ -16,3 +16,19 @@ func ValueCounts(arr []interface{}) map[interface{}]int {
 
 	return valueCounts
 }
+
+type funcMapType func(val interface{}) (interface{}, error)
+
+// Map will map each element of the series to the given function and
+// will return a new series
+func Map(series []interface{}, mapper funcMapType) ([]interface{}, error) {
+	newSeries := make([]interface{}, 0, len(series))
+	for _, val := range series {
+		mappedVal, err := mapper(val)
+		if err != nil {
+			return nil, err
+		}
+		newSeries = append(newSeries, mappedVal)
+	}
+	return newSeries, nil
+}
