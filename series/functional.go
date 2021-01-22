56c729a8d3c5ff55fc1fe1f2fbbc8165908cc30f
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-01-23 00:33:25 +0530

    Added tests for series map function

--- series/functional.go
+++ series/functional.go
@@ -4,14 +4,16 @@ type funcMapType func(val interface{}) (interface{}, error)
 
 // Map will map each element of the series to the given function and
 // will return a new series
-func (s *Series) Map(mapper funcMapType) ([]interface{}, error) {
-	newSeries := make([]interface{}, 0, s.Len())
+func (s *Series) Map(mapper funcMapType) (*Series, error) {
+	seriesData := make([]interface{}, 0, s.Len())
 	for _, val := range s.Data {
 		mappedVal, err := mapper(val)
 		if err != nil {
 			return nil, err
 		}
-		newSeries = append(newSeries, mappedVal)
+		seriesData = append(seriesData, mappedVal)
 	}
-	return newSeries, nil
+
+	newSeries := Series{column: s.column, Data: seriesData}
+	return &newSeries, nil
 }
---
+++ series/functional_test.go
@@ -0,0 +1,33 @@
+package series
+
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"math"
+	"testing"
+)
+
+func TestSeries_Map(t *testing.T) {
+	mapper := func(a interface{}) (interface{}, error) {
+		floatVal, ok := a.(float64)
+		if !ok {
+			return nil, errors.New("only float values can be included")
+		}
+		return math.Pow(floatVal, 2), nil
+	}
+
+	s1 := NewSeries("col", 12.23, 23.11, 14.65, 6.33, 11.90)
+	s2 := NewSeries("col", 12, 23, 14, 6, 11)
+	expected := NewSeries("col", 149.5729, 534.0721, 214.6225, 40.0689, 141.61)
+
+	// assert that the squared function mapper works for an float series
+	actual, err := s1.Map(mapper)
+	assert.Equal(t, expected, actual, "mapper returns correct values")
+	assert.Nil(t, err, "error is nil")
+
+	// assert that the squared function mapper doesn't work for a float series
+	actual, err = s2.Map(mapper)
+	assert.Nil(t, actual, "series is nil")
+	assert.EqualError(t, err, "only float values can be included",
+		"mapper returns errors as expected")
+}
