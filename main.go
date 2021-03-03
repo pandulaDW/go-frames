f1b4a3be58d240a9dfc04ab39e6535ba8a38ef94
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-03-03 23:16:06 +0530

    Added and tested dataframe apply to row method

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/pandulaDW/go-frames/dataframes"
 	"github.com/pandulaDW/go-frames/ioread"
-	"github.com/pandulaDW/go-frames/series"
+	"math"
 )
 
 func main() {
@@ -13,12 +13,19 @@ func main() {
 		return
 	}
 
-	col1 := series.NewSeries("col1", 12, 34, 54, 65, 90)
-	col2 := series.NewSeries("col2", "foo", "bar", "raz", "apple", "orange")
-	col3 := series.NewSeries("col3", 54.31, 1.23, 45.6, 23.12, 23.2)
-	col4 := series.NewSeries("col4", true, false, true, true, false)
-	df = dataframes.NewDataFrame(col1, col2, col3, col4)
-	df.SetIndex("col2")
+	//df.Drop("species")
+	roundTo2 := func(val interface{}) (interface{}, error) {
+		floatVal, ok := val.(float64)
+		if !ok {
+			return nil, errors.New("not a good value")
+		}
+		return math.Round(floatVal), nil
+	}
 
-	fmt.Println(df)
+	df, err = df.ApplyToRows(roundTo2)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(df.Head(5))
+	}
 }
