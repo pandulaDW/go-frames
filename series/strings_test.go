22b2db452ccc459263ebf4de99733e2c80210650
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-03-20 12:53:00 +0530

    Added tests for string methods

---
+++ series/strings_test.go
@@ -0,0 +1,44 @@
+package series
+
+import (
+	"github.com/pandulaDW/go-frames/base"
+	"github.com/pandulaDW/go-frames/errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestHelperStringMethods(t *testing.T) {
+	// assert that function returns an error if invalid series is entered
+	s := NewSeries("test", 12, 43, 11, 10)
+	assert.PanicsWithError(t, errors.IncorrectDataType(base.Object).Error(), func() {
+		helperStringMethods(s, func(val string) string {
+			return ""
+		})
+	})
+
+	s = NewSeries("test", "foo", "bar", "baz")
+	s.Data[2] = 5
+	assert.PanicsWithError(t, errors.InvalidSeriesValError(5, 2, s.column.Name).Error(), func() {
+		helperStringMethods(s, func(val string) string {
+			return ""
+		})
+	})
+}
+
+func TestSeries_Lower(t *testing.T) {
+	// assert that function returns correctly lowered series
+	s := NewSeries("test", "foo", "BAR", "BaZ")
+	assert.Equal(t, NewSeries("test", "foo", "bar", "baz"), s.Lower())
+}
+
+func TestSeries_Upper(t *testing.T) {
+	// assert that function returns correctly upper series
+	s := NewSeries("test", "foo", "BAR", "BaZ")
+	assert.Equal(t, NewSeries("test", "FOO", "BAR", "BAZ"), s.Upper())
+}
+
+func TestSeries_Capitalized(t *testing.T) {
+	// assert that function returns correctly upper series
+	s := NewSeries("test", "foo", "BAR", "BaZ")
+	assert.Equal(t, NewSeries("test", "Foo", "BAR", "BaZ"), s.Capitalized())
+}
