36002818e9b1d188fb3ca5f7f5c09b8514c4ffbc
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-04-02 09:18:06 +0530

    Finish add tests

--- series/ops_test.go
+++ series/ops_test.go
@@ -4,6 +4,7 @@ import (
 	"github.com/pandulaDW/go-frames/errors"
 	"github.com/stretchr/testify/suite"
 	"testing"
+	"time"
 )
 
 type opsTestSuite struct {
@@ -20,6 +21,7 @@ func (suite *opsTestSuite) SetupTest() {
 	suite.SFloat = NewSeries("col", 43.53, 21.1, 32.54, 65.75, nil)
 	suite.SObject = NewSeries("col", "foo", "bar", "baz")
 	suite.sDateTime = NewSeries("col", "2005-01-25", "", "2012-02-05", "1998-11-25", "2001-12-15")
+	_ = suite.sDateTime.CastAsTime("2006-01-02")
 }
 
 func (suite *opsTestSuite) TestHelperCrud() {
@@ -58,7 +60,7 @@ func (suite *opsTestSuite) TestHelperCrud() {
 		sFloat.Add(3.2)
 	})
 
-	// Object ---------------------------------------
+	// Object ------------------------------------------
 	// assert that function panics for incorrectly typed values for object
 	suite.PanicsWithError(errors.IncorrectTypedParameter("val", "string").Error(), func() {
 		suite.SObject.Add(23)
@@ -70,8 +72,44 @@ func (suite *opsTestSuite) TestHelperCrud() {
 	suite.PanicsWithError(errors.InvalidSeriesValError(3.5, 2, "col").Error(), func() {
 		sObject.Add("foo")
 	})
+
+	// DateTime ----------------------------------------
+	// assert that function panics for incorrectly typed values for datetime
+	suite.PanicsWithError(errors.IncorrectTypedParameter("val", "time.Duration").Error(), func() {
+		suite.sDateTime.Add(23)
+	})
+
+	// assert that function panics if invalid series value is encountered
+	sDateTime := suite.sDateTime.DeepCopy()
+	sDateTime.Data[2] = "foo"
+	suite.PanicsWithError(errors.InvalidSeriesValError(3.5, 2, "col").Error(), func() {
+		sDateTime.Add(1 * time.Second)
+	})
+}
+
+func (suite *opsTestSuite) TestSeries_Add() {
+	// assert that function correctly returns an added series for int types
+	suite.Equal(NewSeries("add(col, 4)", 447, 58, nil, 94, 52, 86), suite.SInt.Add(4))
+
+	// assert that function correctly returns an added series when passed a series
+	suite.Equal(NewSeries("add(col, test)", 445, 57, nil, 92, 52, 88), suite.SInt.Add(
+		NewSeries("test", 2, 3, 1, 2, 4, 6)))
+
+	// assert that function correctly returns an added series float types
+	suite.Equal(NewSeries("add(col, 1.2)", 44.730000000000004, 22.3, 33.74, 66.95, nil),
+		suite.SFloat.Add(1.2))
+
+	// assert that function correctly returns an added series object types
+	suite.Equal(NewSeries("add(col, -x)", "foo-x", "bar-x", "baz-x"), suite.SObject.Add("-x"))
+
+	// assert that function correctly returns an added series datetime types
+	expected := NewSeries("add(col, 24h0m0s)", "2005-01-26", "", "2012-02-06", "1998-11-26", "2001-12-16")
+	_ = expected.CastAsTime("2006-01-02")
+	suite.Equal(expected, suite.sDateTime.Add(24*time.Hour))
 }
 
 func TestOpsTestSuite(t *testing.T) {
 	suite.Run(t, new(opsTestSuite))
 }
+
+// suite.sDateTime = NewSeries("col", "2005-01-25", "", "2012-02-05", "1998-11-25", "2001-12-15")
