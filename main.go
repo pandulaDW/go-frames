f80db417bcc43e0596c2d8886eb16806122a6184
Pandula Weerasooriya <39450768+pandulaDW@users.noreply.github.com>
2021-05-06 08:24:14 +0530

    Added compose with AND method

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/pandulaDW/go-frames/ioread"
+	"github.com/pandulaDW/go-frames/series"
 	"log"
 	"time"
 )
@@ -25,8 +26,15 @@ func main() {
 
 	notYangon := df.Col("City").Eq("Yangon").NOT()
 
-	df = df.FilterBySeries(notYangon)
+	t1, _ := time.Parse("2006-01-02", "2019-03-24")
+	t2, _ := time.Parse("2006-01-02", "2019-03-28")
 
+	isGreater := df.Col("DateTime").Gt(t1)
+	isLower := df.Col("DateTime").Lt(t2)
+
+	mask := series.ComposeWithAnd(notYangon, isGreater, isLower)
+
+	df = df.FilterBySeries(mask)
 	fmt.Println(df)
 
 	fmt.Println("time took: ", time.Since(start))
--- series/functional.go
+++ series/functional.go
@@ -1,7 +1,9 @@
 package series
 
 import (
+	"fmt"
 	"github.com/pandulaDW/go-frames/base"
+	"github.com/pandulaDW/go-frames/errors"
 	"github.com/pandulaDW/go-frames/helpers"
 )
 
@@ -24,4 +26,36 @@ func (s *Series) Apply(mapper base.ApplyFunc) (*Series, error) {
 	colName := helpers.FunctionNameWrapper(helpers.GetFunctionName(mapper), s.column.Name)
 	newSeries := NewSeries(colName, seriesData...)
 	return newSeries, nil
+}
+
+// ComposeWithAnd takes in variadic number of base.Bool Series and return a single base.Bool Series
+// by applying AND operation to all the provided Series.
+//
+// The function panics if Series with mismatched number of rows or wrongly typed Series are given.
+func ComposeWithAnd(sBool ...*Series) *Series {
+	initLen := sBool[0].Len()
+	data := make([]interface{}, initLen)
+
+	for _, series := range sBool {
+		if series.Len() != initLen {
+			err := fmt.Errorf("%s is invalid. %s", series.column.Name,
+				errors.MismatchedNumOfRows(initLen, series.Len()).Error())
+			panic(err)
+		}
+	}
+
+	for i := 0; i < initLen; i++ {
+		val := true
+		for _, series := range sBool {
+			sVal, ok := series.Data[i].(bool)
+			if !ok {
+				panic(errors.InvalidSeriesValError(series.Data[i], i, series.column.Name))
+			}
+			val = val && sVal
+		}
+		data[i] = val
+	}
+
+	newS := &Series{Data: data, column: base.Column{Name: "compose-with-and", Dtype: base.Bool}}
+	return newS
 }
